3c426e7d74690349e7f66718526e2fdec7b7dbf6
Saad100912 <arsaad.2016@gmail.com>
2021-12-01 14:02:44 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.801m1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("portfolio");
+        const projectCollection = database.collection("projects");
+
+        app.get("/projects", async (req, res) => {
+            const projects = projectCollection.find({});
+            const result = await projects.toArray();
+            res.json(result);
+        });
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("Portfolio Server is running");
+});
+app.listen(port, () => {
+    console.log("Portfolio Server is running on:", port);
+});
